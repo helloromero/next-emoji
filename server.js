ec8046cf734a3869e79e7a54033f87944a2ea453
helloromero <romero.mckay@bglgroup.co.uk>
2022-01-25 15:55:10 +0000

    next-emoji first push

---
+++ server.js
@@ -0,0 +1,34 @@
+const app = require('express')()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const next = require('next')
+
+const dev = process.env.NODE_ENV !== 'production'
+const nextApp = next({ dev })
+const nextHandler = nextApp.getRequestHandler()
+
+let port = 3000
+let history = ['🥝', '🍓', '🫐', '🍋', '🍑', '🍆', '🍊', '🥔']
+
+io.on('connection', (socket) => {
+
+  // sents history to all local users
+  io.emit('history', history)
+
+  // updates history and sends new data to all local users
+  socket.on('emojiState', (data) => {
+    history = data
+    io.emit('emojiState', data)
+  });
+});
+
+nextApp.prepare().then(() => {
+  app.get('*', (req, res) => {
+    return nextHandler(req, res)
+  })
+
+  server.listen(port, (err) => {
+    if (err) throw err
+    console.log(`-- Ready on http://localhost:${port}`)
+  })
+})
