0d73a39bcaaad46758c148cb186646b097a506b7
helloromero <romero.mckay@bglgroup.co.uk>
2022-01-27 12:28:00 +0000

    added in simple response test

--- pages/index.js
+++ pages/index.js
@@ -4,23 +4,34 @@ import io from 'socket.io-client'
 let socket = io();
 
 export default function Foo(props) {
-    const emojis = ['🥝', '🍓', '🫐', '🍋', '🍑', '🍆', '🍊', '🥔']
+    const emojis = ['🥝', '🍑', '🫐', '🍋', '🍓', '🍆', '🍊', '🥔']
     const [emojiArray, setEmojiArray] = useState(emojis)
+    const [time, setTime] = useState(0)
 
+  // pushes emoji state to server
   function push(emojiState) {
     socket.emit('emojiState', emojiState)
     socket.on('emojiState', function (emoji) {
       setEmojiArray(emoji)
     });
   }
+
+  // updates local emoji state
   function updateEmojiArrays(emojiIndex) {
+    const d = new Date();
+    let ms = d.getMilliseconds();
     let newState = [ ...emojiArray ];
     newState[emojiIndex] += emojis[emojiIndex]
     push(newState)
+    // push and update current time
+    socket.emit('time', ms)
+    socket.on('time', function (time) {
+      setTime(time)
+    });
   }
 
+  // updates history for local user on load
   useEffect(() => {
-    // updates history for local user on load
     socket.on('history', function (history) {
       push(history)
     });
@@ -28,7 +39,7 @@ export default function Foo(props) {
 
   return (
     <div className={styles['button-wrapper']}>
-      {/* <i>some emojis on a page that you can click lol</i> */}
+      <span className={styles['time']}>{time}</span>
       {emojiArray.map((img, index) => 
         <button key={index} onClick={() => updateEmojiArrays(index)}>{img}</button>
       )}
--- server.js
+++ server.js
@@ -8,17 +8,26 @@ const nextApp = next({ dev })
 const nextHandler = nextApp.getRequestHandler()
 
 let port = 3000
-let history = ['🥝', '🍓', '🫐', '🍋', '🍑', '🍆', '🍊', '🥔']
+let history = ['🥝', '🍑', '🫐', '🍋', '🍓', '🍆', '🍊', '🥔']
 
 io.on('connection', (socket) => {
 
-  // sents history to all local users
+  // sents history back to all local users
   io.emit('history', history)
 
   // updates history and sends new data to all local users
   socket.on('emojiState', (data) => {
     history = data
     io.emit('emojiState', data)
+  });
+
+  // send respnse time back to local 
+  socket.on('time', (data) => {
+    const d = new Date()
+    const ms = d.getMilliseconds()
+    let duration = ms - data
+    duration = (duration < 0) ? 'over 1 second 😢' : duration += 'ms'
+    io.emit('time', duration)
   });
 });
 
